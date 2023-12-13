f6371a569ee7f6ebd57fc93f7d157231c32a1c33
AlarconCode <julian.alarcon81@gmail.com>
2023-12-13 20:16:16 +0100

    añadido dominio https://cafeteria-cocoa-torrijos.vercel.app a cors

--- api/app.js
+++ api/app.js
@@ -13,7 +13,7 @@ app.set('PORT', process.env.PORT || 4000)
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://app-cocoa.vercel.app', 'https://cafeteria-cocoa.vercel.app', 'https://cafeteria-cocoa.onrender.com' ],
+  origin: ['http://localhost:5173', 'https://app-cocoa.vercel.app', 'https://cafeteria-cocoa.vercel.app', 'https://cafeteria-cocoa.onrender.com', 'https://cafeteria-cocoa-torrijos.vercel.app' ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }))
