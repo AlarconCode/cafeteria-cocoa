63a33e171fa08429f7cbd8cec28d2532968c06de
AlarconCode <julian.alarcon81@gmail.com>
2023-12-14 18:29:34 +0100

    config cookie sameSite strict

--- api/src/controller/auth.controller.js
+++ api/src/controller/auth.controller.js
@@ -50,7 +50,7 @@ export const register = async (req, res) => {
       httpOnly: true,
       maxAge: 3 * 24 * 60 * 60 * 1000,
       secure: true,
-      sameSite: 'none',
+      sameSite: 'strict',
     })
     .json({
       error: false,
@@ -134,7 +134,7 @@ export const login = async (req, res) => {
         httpOnly: true,
         maxAge: 3 * 24 * 60 * 60 * 1000,
         secure: true,
-        sameSite: 'none'
+        sameSite: 'strict'
       })
       .json({
         error: false,
@@ -167,7 +167,7 @@ export const logout = async (req, res) => {
         httpOnly: true,
         maxAge: 0,
         secure: true,
-        sameSite: 'none'
+        sameSite: 'strict'
       }).status(200).json({ message: 'You are logged out!' });
   
   } catch (err) {
